197e208ec5a5910b137edeb8b2e032a591f2e43f
Ragnar <raxzu@live.com>
2025-07-27 16:31:06 +0300

    mobile done

--- app.js
+++ app.js
@@ -105,4 +105,29 @@ document.querySelectorAll(".slideshow").forEach(slideshow => {
   }, 4000);
 });
 
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
 
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
